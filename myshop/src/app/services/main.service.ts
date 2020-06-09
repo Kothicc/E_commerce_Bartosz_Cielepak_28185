8f205bd2fb8685bfb1f78e35e55a2a27444c2e11
unknown <kothiclunaris@gmail.com>
2020-06-09 16:01:39 +0200

    Laboratorium 7

--- myshop/src/app/services/main.service.ts
+++ myshop/src/app/services/main.service.ts
@@ -17,6 +17,7 @@ export class MainService {
   apiPath = 'http://jakubadamus.cba.pl/xhr.php?'; // Ścieżka do naszego api
 
   cart = [];
+  orders = null;
 
   getProducts(productsRequest) { //  Pobiera produkty poprzez API
     const s = new Promise((resolve, reject) => {
@@ -25,7 +26,7 @@ export class MainService {
       console.log(this.apiPath + SQL);
       xhttp.open('GET', this.apiPath + SQL, true);
       xhttp.send();
-      xhttp.onreadystatechange = function () {
+      xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
           const resultObject = JSON.parse(xhttp.responseText);
 
@@ -47,11 +48,11 @@ export class MainService {
   addProduct(newProduct) {
     const s = new Promise((resolve, reject) => {
       const xhttp = new XMLHttpRequest();
-      let request = { action: 'addProduct', newProduct: newProduct }
+      const request = { action: 'addProduct', newProduct };
       const SQL = ('object=' + encodeURIComponent(JSON.stringify(request)));
-      console.log(this.apiPath + SQL);xhttp.open('GET', this.apiPath + SQL, true);
+      console.log(this.apiPath + SQL); xhttp.open('GET', this.apiPath + SQL, true);
       xhttp.send();
-      xhttp.onreadystatechange = function () {
+      xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
           const resultObject = JSON.parse(xhttp.responseText);
 
@@ -72,12 +73,12 @@ export class MainService {
   removeProduct(id) {
     const s = new Promise((resolve, reject) => {
       const xhttp = new XMLHttpRequest();
-      let request = { action: 'removeProduct', id: id }
+      const request = { action: 'removeProduct', id };
       const SQL = ('object=' + encodeURIComponent(JSON.stringify(request)));
       console.log(this.apiPath + SQL);
       xhttp.open('GET', this.apiPath + SQL, true);
       xhttp.send();
-      xhttp.onreadystatechange = function () {
+      xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
           const resultObject = JSON.parse(xhttp.responseText);
 
@@ -95,4 +96,30 @@ export class MainService {
       console.log('Coś poszło nie tak podczas usuwania produktu!');
     });
   }
+    getOrders() {
+    const s = new Promise((resolve, reject) => {
+    const xhttp = new XMLHttpRequest();
+    const request = { action: 'getOrders' };
+    const SQL = ('object=' + encodeURIComponent(JSON.stringify(request)));
+    console.log(this.apiPath + SQL);
+    xhttp.open('GET', this.apiPath + SQL, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+    const resultObject = JSON.parse(xhttp.responseText);
+    if (resultObject !== null) {
+    resolve(resultObject);
+    } else {
+    reject('Failed');
+    }
+    }
+    };
+    });
+    s.then((onmessage: any) => {
+    console.log('Pomyślnie pobrano zamówienia!');
+    this.orders = onmessage;
+    }).catch((onmessage) => {
+    console.log('Coś poszło nie tak podczas pobierania zamówień!');
+    });
+    }
 }
